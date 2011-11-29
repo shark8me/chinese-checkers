368a4691a1183efb1e38153e6f3bf19139cd9789
Kiran Karkera <kiran.karkera@gmail.com>
2011-11-29 23:27:13 +0530

    added fn to mark duplicates

--- src/chinese_checkers/forest.clj
+++ src/chinese_checkers/forest.clj
@@ -23,7 +23,6 @@
             (map #(-> % zip/node :self ) 
                  (zt/leafnodes (zt/mzipper xroot))))))
 
-(uniq-leaves zt/mroot)
 
 (defn leaf-edit [[loc shet]]
   (do ;(println "le" (zip/node loc))
@@ -35,8 +34,7 @@
                  (zip/edit loc efn)  
                  loc)) newshet ])))    
     
-(-> [(zt/mzipper zt/mroot) #{}] leaf-edit leaf-edit 
-  leaf-edit leaf-edit leaf-edit leaf-edit leaf-edit leaf-edit zip/end?)
+
 
 (defn leaf-walk-edit [editfn iterstart]
   "walks the tree, 
@@ -45,12 +43,40 @@
                  (iterate editfn iterstart))))
 
 (def nroot {:self 20 :cren [ 
-                            {:self 10 :cren [{:self 11} {:self 11}]} 
+                            {:self 10 :cren [{:self 11} {:self 14}]} 
                             {:self 30 :cren [{:self 11} {:self 32}]}
                             ]})
+(def nroot1 {:self 29 :cren [ 
+                            {:self 10 :cren [{:self 12} {:self 14}]} 
+                            {:self 30 :cren [{:self 16} {:self 17}]}
+                            ]})
+(def nroot2 {:self 50 :cren [ 
+                            {:self 10 :cren [{:self 14} {:self 15}]} 
+                            {:self 30 :cren [{:self 16} {:self 14}]}
+                            {:self 31 :cren [{:self 16} {:self 15}]}
+                            ]})
 
+(def remove-duplicate-leaves (partial leaf-walk-edit leaf-edit))
+  
 ;set contains only unique leaf node values.
-(t/is (= #{32 11} (last (leaf-walk-edit leaf-edit [(zt/mzipper nroot) #{}]))))
+(t/is (= #{32 14 11} 
+         (last 
+           (remove-duplicate-leaves [(zt/mzipper nroot) #{}]))))
 
+(t/is (= #{14 15 16} 
+         (last 
+           (remove-duplicate-leaves [(zt/mzipper nroot2) #{14}]))))
 
+;seq of trees
+;(def for1 [nroot nroot1 nroot2])
+    
+(defn redfn [[lst-of-trees aset ] ith-tree]
+  (let [ret (remove-duplicate-leaves [ith-tree aset])]  
+    ;(println x aset )    
+    [(conj lst-of-trees (first ret)) (last ret)]      
+    ))
 
+(t/is (= #{32 11 12 14 15 16 17}
+         (last (reduce redfn [[] #{}] 
+                       (map zt/mzipper [nroot nroot1 nroot2])))))
+ 
